fa7ed252e1a2b5477ba244ef2de78c30aaf90648
marcello.messori <marcello.messori@dquid.com>
2014-07-10 16:15:44 +0200

    android: bugfix and improved styles

--- android/MyFirstAppMvnEclipse/src/main/java/com/marcellomessori/HelloAndroidActivity.java
+++ android/MyFirstAppMvnEclipse/src/main/java/com/marcellomessori/HelloAndroidActivity.java
@@ -1,6 +1,7 @@
 package com.marcellomessori;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -34,19 +35,34 @@ public class HelloAndroidActivity extends Activity {
 		inflater.inflate(R.menu.activity_android_hello_actions, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle presses on the action bar items
-	    switch (item.getItemId()) {
-	        case R.id.action_search:
-	            //openSearch();
-	            return true;
-	        case R.id.action_settings:
-	            //openSettings();
-	            return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+		case R.id.action_search:
+			openSearch();
+			return true;
+		case R.id.action_settings:
+			openSettings();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private void openSearch() {
+		alertNotImplemented("Search");
+	}
+
+	private void openSettings() {
+		alertNotImplemented("Settings");		
+	}
+	
+	private void alertNotImplemented(String title) {
+		new AlertDialog.Builder(this).setTitle(title)
+				.setMessage("Not implemented")
+				.setIcon(android.R.drawable.ic_dialog_alert)
+				.show();
 	}
 }
