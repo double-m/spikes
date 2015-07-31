960092769aeebeb329110193c032781fb6e42455
Marcello Messori <marcello.mm.messori@gmail.com>
2015-07-31 17:33:28 +0200

    ongoing zend-framework_album: added a controller and the first view

---
+++ zend-framework_album/module/Album/config/module.config.php
@@ -0,0 +1,44 @@
+<?php
+
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Album\Controller\Album' => 'Album\Controller\AlbumController',
+        ),
+    ),
+    
+    'router' => array(
+        'routes' => array(
+            'album' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/album/[/:action][/:id]',
+                    'contraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id' => '[0-9]+'
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Album/Controller/Album',
+                        'action' => 'index'
+                    )
+                )
+            )
+        )
+    ),
+    
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => array(
+            'album/album/index'       => __DIR__ . '/../view/album/album/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            'album' => __DIR__ . '/../view',
+        ),
+    ),
+);
