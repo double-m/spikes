dec5dfe7a996f4c07b23e1fd7704c497a34c79ee
Marcello Messori <marcello.mm.messori@gmail.com>
2015-08-17 09:58:32 +0200

    ongoing zend-framework_album: bugfixes

--- zend-framework_album/module/Album/src/Album/Controller/AlbumController.php
+++ zend-framework_album/module/Album/src/Album/Controller/AlbumController.php
@@ -3,7 +3,6 @@
 namespace Album\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
-use Zend\View\Model\ViewModel;
 use Album\Model\Album;
 use Album\Form\AlbumForm;
 
--- zend-framework_album/public/index.php
+++ zend-framework_album/public/index.php
@@ -3,7 +3,7 @@
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
-if ($_SERVER['APPLICATION_ENV'] === 'development') {
+if (filter_input(INPUT_SERVER, $_SERVER['APPLICATION_ENV']) === 'development') {
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
 }
@@ -12,7 +12,7 @@ chdir(dirname(__DIR__));
 
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server') {
-    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    $path = realpath(__DIR__ . parse_url(filter_input(INPUT_SERVER, $_SERVER['REQUEST_URI']), PHP_URL_PATH));
     if (__FILE__ !== $path && is_file($path)) {
         return false;
     }
