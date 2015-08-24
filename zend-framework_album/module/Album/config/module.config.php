9318fc2d285165e665f5828b6aa7ac16a5a7dbf7
Marcello Messori <marcello.mm.messori@gmail.com>
2015-08-24 18:48:46 +0200

    ongoing zend-framework_album: added a test suite

--- zend-framework_album/module/Album/config/module.config.php
+++ zend-framework_album/module/Album/config/module.config.php
@@ -33,7 +33,7 @@ return array(
             'album' => array(
                 'type' => 'segment',
                 'options' => array(
-                    'route' => '/album/[/:action][/:id]',
+                    'route' => '/album[/:action][/:id]',
                     'contraints' => array(
                         'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
                         'id' => '[0-9]+'
