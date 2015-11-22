3ae1b73de5156f8bf4640b606e8fd636d80b55f0
Marcello Messori <marcello.mm.messori@gmail.com>
2015-11-22 18:48:16 +0100

    ongoing nodeschool/learnyoumongo

---
+++ nodeschool/learnyoumongo/6_update.js
@@ -0,0 +1,22 @@
+var MongoClient = require('mongodb').MongoClient
+  , dbName = process.argv[2]
+  , url = 'mongodb://localhost:27017/' + dbName
+  , username = 'tinatime'
+  , newAge = 40;
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+
+    var users = db.collection('users');
+    users.update({
+        username: username
+    }, {
+        $set: {
+            age: newAge
+        }
+    }, function(err, data) {
+        if (err) throw err;
+
+        db.close();
+    });
+});
