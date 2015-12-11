c41fddee548849e645a54d6c1f5a0b9972050bab
Marcello Messori <marcello.mm.messori@gmail.com>
2015-12-11 16:03:08 +0100

    added feature: insert todo task into db and refresh the html list

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express')
+  , bodyParser = require('body-parser')
+  , os = require('os')
   , MongoClient = require('mongodb').MongoClient;
 
 var app = express()
@@ -7,14 +9,39 @@ var app = express()
 
 app.use(express.static('./public'));
 
-app.get('/todo', function(req, res, next) {
+app.use( bodyParser.json() );
+
+app.get('/todos', function(req, res, next) {
     var db = app.locals.db;
     
-    var cursor = db.collection('todo').find();
+    var cursor = db.collection('todos').find();
     
     cursor.toArray(function(req, todos) {
         res.send(todos);  
     });
+});
+
+app.post('/todos', function(req, res, next) {
+    var task = req.body.task;
+    
+    if (!task) {
+        outputMessage = 'not inserted new task: ' + task + os.EOL;
+        res.status(400).send(outputMessage);
+        console.log(outputMessage);
+        return false;
+    }
+    
+    var db = app.locals.db;
+    db.collection('todos').insert({
+        task: task,
+        completed: false
+    }, function(err, result) {
+        if(err) throw err;
+        outputMessage = 'stored a new task: "' + task + '"' + os.EOL;
+        res.status(201).send(outputMessage);
+        console.log(outputMessage);
+    });
+
 });
 
 MongoClient.connect(mongoUrl, function(err, db) {
--- public/app.js
+++ public/app.js
@@ -3,8 +3,37 @@
 angular.module('meanTodo', [])
     .controller('mainCtrl', function($http) {
         var main = this;
-      
-        $http.get('/todo').then(function(res) {
-            main.todos = res.data;
-        });
+
+        main.addTodo = function() {
+            if (!main.newTodoText) return false;
+
+            console.log('sending', main.newTodoText);
+            
+            $http({
+                method: 'POST',
+                url: '/todos',
+                data: {
+                    task: main.newTodoText,
+                    completed: false
+                }
+            }).then(function (res) {
+                if (res.status === 201) {
+                    main.getTodos();
+                }
+
+                var consoleMessage = (res.status === 201)
+                    ? 'new task "' + main.newTodoText + '" has been stored'
+                    : 'server response was "' + res.data + '" with http status ' + res.status;
+                console.log(consoleMessage);
+            });
+        };
+
+        main.getTodos = function() {
+            $http.get('/todos').then(function(res) {
+                main.todos = res.data;
+            });
+        }
+        
+        // initial population
+        main.getTodos();
     });
