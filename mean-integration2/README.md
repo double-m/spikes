33fa2263b9eeb565cb52a056ddbedcc56cb47102
Marcello Messori <m.messori@esis-italia.com>
2016-10-21 00:28:12 +0200

    [mean-integration2] added /api/products with GET and POST methods

--- mean-integration2/README.md
+++ mean-integration2/README.md
@@ -26,3 +26,10 @@ npm install --save mongodb
 npm install --save-dev nodemon
 node_modules/nodemon/bin/nodemon.js -e js,html bin/www
 ```
+
+### Server APIs: GET|POST /api/product
+
+```
+curl localhost:3000/api/products | jq  -r '.'
+curl -XPOST localhost:3000/api/products -H 'Content-Type: application/json' --data '{"product": {"name": "Prodotto 1", "code": "p1"}}'
+```
--- mean-integration2/server/app.js
+++ mean-integration2/server/app.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const express = require('express')
+    , bodyParser = require('body-parser')
+    , os = require('os');
 
 app = express();
 
-app.get('/', function(request, response) {
-  var db = app.locals.db;
+app.use(bodyParser.json());
 
-  var cursor = db.collection('fixtures').find({'key': {$eq: 'greetingMessage'}});
+app.get('/', function(request, response){
+  var cursor = app.locals.db.collection('fixtures').find({'key': {$eq: 'greetingMessage'}});
 
   cursor.toArray(function(err, fixtures) {
     if (err) {
@@ -14,6 +16,38 @@ app.get('/', function(request, response) {
       response.send(fixtures[0].message);
     }
   });
+});
+
+app.get('/api/products', function(request, response) {
+  var cursor = app.locals.db.collection('products').find({});
+
+  cursor.toArray(function(err, products) {
+    if (err) throw err;
+
+    response.send(products);
+  });
+});
+
+app.post('/api/products', function(request, response) {
+  var product = request.body.product;
+
+  if (!product) {
+      var outputMessage = 'not inserted new product: ' + product + os.EOL;
+      response.status(400).send(outputMessage);
+      console.log(outputMessage);
+      return false;
+  }
+
+  app.locals.db.collection('products').insert({
+      product: product
+  }, function(err, result) {
+      if(err) throw err;
+
+      var outputMessage = 'stored a new product: "' + product + '"' + os.EOL;
+      response.status(201).send(outputMessage);
+      console.log(outputMessage);
+  });
+
 });
 
 module.exports = app;
